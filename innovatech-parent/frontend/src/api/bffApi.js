e30f47683c4ed28130db589702e522c8b917ceff
Jonathan Lillo <jon.lillo@duocuc.cl>
2026-06-04 23:41:13 -0400

    Conecta frontend con BFF mediante API Gateway

--- innovatech-parent/frontend/src/api/bffApi.js
+++ innovatech-parent/frontend/src/api/bffApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const bffApi = axios.create({
-  baseURL: "http://localhost:8085/api/v1/bff",
+  baseURL: "http://localhost:8090/api/v1/bff",
 });
 
 export default bffApi;
